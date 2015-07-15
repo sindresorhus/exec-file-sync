ac7d0283d1a8154dc58f51fb0013c67cbaf2d909
Sindre Sorhus <sindresorhus@gmail.com>
2015-07-16 01:17:18 +0200

    cleanup and improve #2

--- index.js
+++ index.js
@@ -1,97 +1,80 @@
 'use strict';
 var spawnSync = require('spawn-sync');
 var childProcess = require('child_process');
-var util = require('util');
-
-module.exports = childProcess.execFileSync || execFileSync;
-
-function isObject(arg) {
-  return typeof arg === 'object' && arg !== null;
-}
-
-var _deprecatedCustomFds = util.deprecate(function(options) {
-  options.stdio = options.customFds.map(function(fd) {
-    return fd === -1 ? 'pipe' : fd;
-  });
-}, 'child_process: customFds option is deprecated, use stdio instead.');
-
-function _convertCustomFds(options) {
-  if (options && options.customFds && !options.stdio) {
-    _deprecatedCustomFds(options);
-  }
-}
-
+var isObject = require('is-obj');
+var objectAssign = require('object-assign');
 
 function normalizeSpawnArguments(file /*, args, options*/) {
-  var args, options;
+	var args;
+	var options;
 
-  if (Array.isArray(arguments[1])) {
-    args = arguments[1].slice(0);
-    options = arguments[2];
-  } else if (arguments[1] !== undefined && !isObject(arguments[1])) {
-    throw new TypeError('Incorrect value of args option');
-  } else {
-    args = [];
-    options = arguments[1];
-  }
+	if (Array.isArray(arguments[1])) {
+		args = arguments[1].slice(0);
+		options = arguments[2];
+	} else if (arguments[1] !== undefined && !isObject(arguments[1])) {
+		throw new TypeError('Incorrect value of args option');
+	} else {
+		args = [];
+		options = arguments[1];
+	}
 
-  if (options === undefined)
-    options = {};
-  else if (!isObject(options))
-    throw new TypeError('options argument must be an object');
+	if (options === undefined) {
+		options = {};
+	} else if (!isObject(options)) {
+		throw new TypeError('options argument must be an object');
+	}
 
-  args.unshift(file);
+	args.unshift(file);
 
-  var env = options.env || process.env;
-  var envPairs = [];
+	var env = options.env || process.env;
+	var envPairs = [];
 
-  for (var key in env) {
-    envPairs.push(key + '=' + env[key]);
-  }
+	for (var key in env) {
+		envPairs.push(key + '=' + env[key]);
+	}
 
-  _convertCustomFds(options);
-
-  return {
-    file: file,
-    args: args,
-    options: options,
-    envPairs: envPairs
-  };
+	return {
+		file: file,
+		args: args,
+		options: options,
+		envPairs: envPairs
+	};
 }
 
 function checkExecSyncError(ret) {
-  if (ret.error || ret.status !== 0) {
-    var err = ret.error;
-    ret.error = null;
+	if (ret.error || ret.status !== 0) {
+		var err = ret.error;
+		ret.error = null;
 
-    if (!err) {
-      var msg = 'Command failed: ' +
-        (ret.cmd ? ret.cmd : ret.args.join(' ')) +
-          (ret.stderr ? '\n' + ret.stderr.toString() : '');
-      err = new Error(msg);
-    }
+		if (!err) {
+			var msg = 'Command failed: ' +
+				(ret.cmd ? ret.cmd : ret.args.join(' ')) +
+				(ret.stderr ? '\n' + ret.stderr.toString() : '');
+			err = new Error(msg);
+		}
 
-    util._extend(err, ret);
-    return err;
-  }
+		objectAssign(err, ret);
+		return err;
+	}
 
-  return false;
+	return false;
 }
 
-function execFileSync(/*command, options*/) {
-  var opts = normalizeSpawnArguments.apply(null, arguments);
-  var inheritStderr = !opts.options.stdio;
+module.exports = childProcess.execFileSync || function (/*command, options*/) {
+	var opts = normalizeSpawnArguments.apply(null, arguments);
+	var inheritStderr = !opts.options.stdio;
 
-  var ret = spawnSync(opts.file, opts.args.slice(1), opts.options);
+	var ret = spawnSync(opts.file, opts.args.slice(1), opts.options);
 
-  if (inheritStderr)
-    process.stderr.write(ret.stderr);
+	if (inheritStderr) {
+		process.stderr.write(ret.stderr);
+	}
 
-  var err = checkExecSyncError(ret);
+	var err = checkExecSyncError(ret);
 
-  if (err)
-    throw err;
-  else
-    return ret.stdout;
-}
-exports.execFileSync = execFileSync;
+	if (err) {
+		throw err;
+	} else {
+		return ret.stdout;
+	}
+};
